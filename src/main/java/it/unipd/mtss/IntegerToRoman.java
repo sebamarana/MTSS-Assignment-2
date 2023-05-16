6ba8b5424c638eef9c7035e08a1f6065b2e67bec
Romeo0212 <romeocalearo@gmail.com>
2023-05-16 16:44:00 +0200

    Modifica IntegerToRoman

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -15,7 +15,8 @@ public class IntegerToRoman {
         }
 
         int numbers[] = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
-        String symbols[] = {"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
+        String symbols[] = {"I","IV","V","IX","X","XL",
+                            "L","XC","C","CD","D","CM","M"};
         String conv = "";
 
         int count = numbers.length - 1;
