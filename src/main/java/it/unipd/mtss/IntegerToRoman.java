23494dffdafc313f4251ad5c6e3e5a87762c9b4a
Romeo0212 <romeocalearo@gmail.com>
2023-05-16 16:40:59 +0200

    Creazione IntegerToRoman
    close #7

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -5,9 +5,28 @@
 
 
 package it.unipd.mtss;
+
+import java.security.InvalidParameterException;
+
 public class IntegerToRoman {
     public static String convert(int number){
-        // TODO
-        return null;
+        if(number <= 0) {
+            throw new InvalidParameterException();
+        }
+
+        int numbers[] = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+        String symbols[] = {"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
+        String conv = "";
+
+        int count = numbers.length - 1;
+        while(count >= 0) {
+            if (number >= numbers[count]) {
+                conv += symbols[count];
+                number -= numbers[count];
+            } else {
+                count--;
+            }
+        }
+        return conv;
     }
 }
